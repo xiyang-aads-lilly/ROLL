664fded38bf075bb4b3cbd86154190fc41316bf2
xiongshaopan.xsp <xiongshaopan.xsp@alibaba-inc.com>
2025-07-11 11:15:10 +0800

    (feat): set default max_length/max_new_tokens to 8192.

--- roll/configs/generating_args.py
+++ roll/configs/generating_args.py
@@ -31,11 +31,11 @@ class GeneratingArguments:
         metadata={"help": "Number of beams for beam search. 1 means no beam search."},
     )
     max_length: Optional[int] = field(
-        default=512,
+        default=8192,
         metadata={"help": "The maximum length the generated tokens can have. It can be overridden by max_new_tokens."},
     )
     max_new_tokens: Optional[int] = field(
-        default=512,
+        default=8192,
         metadata={"help": "The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt."},
     )
     repetition_penalty: Optional[float] = field(
